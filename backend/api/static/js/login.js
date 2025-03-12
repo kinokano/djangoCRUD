7abd61e6ffb1513506b5a35a41f9228eec85b9a5
kinokano <kin2014@hotmail.com>
2025-03-12 16:59:56 -0400

    A

---
+++ backend/api/static/js/login.js
@@ -0,0 +1,18 @@
+async function Login(event){
+    event.preventDefault();
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+
+    console.log(senha)
+    const response = await apiRequest("/api/login/", "POST", {nome: nome, senha: senha}, {"X-CSRFToken": "{{ csrf_token }}"})
+
+    console.log(response)
+    if(response.status == 200){
+        window.location.href = "/home";
+    }else{
+        document.getElementById("mensagem").innerHTML = `<div class="alert alert-danger">${data.message}</div>`;
+    }
+}
+
+
+document.getElementById("loginForm").addEventListener("submit",Login);
--- backend/api/static/js/utils.js
+++ backend/api/static/js/utils.js
@@ -1,21 +1,25 @@
-async function apiFetch(url, method="GET", body=null, headers={}){
+async function apiRequest(url, method = 'GET', body = null, headers = {}) {
     try {
         const config = {
             method,
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
                 ...headers
-            },       
+            },
         };
-        if(body){
-            config.body = JSON.stringify(body)
+
+        if (body) {
+            config.body = JSON.stringify(body);
         }
-        
-        const resposta = await fetch(url, config)
 
-        return resposta.json()
+        const response = await fetch(url, config);
+        if (!response.ok) {
+            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+        }
 
+        return await response.json();
     } catch (error) {
-        
+        console.error('Erro na requisição:', error);
+        return null;
     }
 }
